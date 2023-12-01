317aefafe898268f1c283dc0178415323f412ace
Mohammed Ahmed <72145684+Ahmed4164@users.noreply.github.com>
2023-12-02 01:09:15 +0530

    Delete src/validations directory

--- src/validations/UserInfo.ts
+++
@@ -1,5 +0,0 @@
-// import { userInfo } from "os";
-import { z } from "zod";
-export const userInfoSchema =z.object({
-    landArea: z.number().min(1,{message:"enter ,ore than 1"}).max(10,{message:"enter less tha n 10"})
-})
